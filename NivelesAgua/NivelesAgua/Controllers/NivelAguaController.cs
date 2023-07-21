81441529c6accae8af7883de88f54005dc785102
juancturcios <juancturcios@hotmail.com>
2023-07-21 10:06:50 -0600

    prueba de Stefanini

---
+++ NivelesAgua/NivelesAgua/Controllers/NivelAguaController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NivelesAguaController.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class NivelAguaController : ControllerBase
+	{
+		private readonly ILogger<NivelAguaController> _logger;
+		[HttpPost("GetResult")]
+		public int GetItems(int[] arreglo)
+		{
+			int longitudArreglo = arreglo.Length;
+			int[] izquierda = new int[longitudArreglo];
+			int[] derecha = new int[longitudArreglo];
+			int[] agua = new int[longitudArreglo];
+			int contador = 0;
+			for (int i = 0; i < longitudArreglo; i++)
+			{
+				if(i==0)
+				{
+					izquierda[i] = 0;
+				}
+				else
+				{
+					izquierda[i] = Math.Max(arreglo[i], izquierda[i - 1]);
+				}
+			}
+
+			for (int i = (longitudArreglo-1); i >= 0; i--)
+			{
+				if (i == longitudArreglo-1)
+				{
+					derecha[i] = 0;
+				}
+				else
+				{
+					derecha[i] = Math.Max(arreglo[i], derecha[i + 1]);
+				}
+			}
+
+			for (int i = 0; i < longitudArreglo; i++)
+			{
+				agua[i] = Math.Min(izquierda[i], derecha[i]) - arreglo[i];
+				agua[i]= (agua[i]<0)? 0 : agua[i];
+				contador += agua[i];
+			}
+
+			return contador;
+		}
+		
+	}
+}
